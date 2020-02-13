9ba7f6a0f6edf597ae7f3d45cdd52691036c5fcc
Tor Morten Jensen <tormorten@tormorten.no>
2020-02-13 10:58:15 +0100

    add redis caching

--- src/SpreadsheetServiceProvider.php
+++ src/SpreadsheetServiceProvider.php
@@ -3,6 +3,10 @@
 namespace OSMAviation\Spreadsheet;
 
 use Illuminate\Support\ServiceProvider;
+use Cache\Adapter\Predis\PredisCachePool;
+use Cache\Adapter\Redis\RedisCachePool;
+use Cache\Bridge\SimpleCache\SimpleCacheBridge;
+use PhpOffice\PhpSpreadsheet\Settings as SpreadsheetSettings;
 
 class SpreadsheetServiceProvider extends ServiceProvider
 {
@@ -13,10 +17,49 @@ class SpreadsheetServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->registerCache();
+
         $this->app->bind('spreadsheet', function ($app) {
             return new PhpSpreadsheet();
         });
 
         $this->app->alias('spreadsheet', PhpSpreadsheet::class);
+    }
+
+    public function boot()
+    {
+        $this->registerCache();
+
+        if ($bridge = $this->app['spreadsheet.cacheBridge']) {
+            SpreadsheetSettings::setCache($bridge);
+        }
+    }
+
+    protected function registerCache()
+    {
+        $this->app->bind('spreadsheet.pool', function ($app) {
+            if (!isset($app['redis.connection'])) {
+                return null;
+            }
+
+            $client = $app['redis.connection']->client();
+
+            if ($client instanceof \Redis) {
+                return new RedisCachePool($client);
+            }
+
+            if ($client instanceof \Predis\Client) {
+                return new PredisCachePool($client);
+            }
+
+            return null;
+        });
+
+        $this->app->bind('spreadsheet.cacheBridge', function ($app) {
+            if (!$app['spreadsheet.pool']) {
+                return null;
+            }
+            return new SimpleCacheBridge($app['spreadsheet.pool']);
+        });
     }
 }
